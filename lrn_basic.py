52ff9373e82de33add771b3e28d609ce7c0ab12e
Codename red <109427846+Sarifs@users.noreply.github.com>
2025-11-24 22:45:48 -0500

    Add basic string operations and function example

---
+++ lrn_basic.py
@@ -0,0 +1,19 @@
+#sub sequence
+str = "expension"
+print(str[2::]) # resulte  = pension
+# find substring
+
+s = "Babylone babies and saletile sisters"
+index = s.find("sister")
+print(index) # return the index of first letter of subsequence postion
+
+#string to int
+
+s = "42"
+num = int(s)
+print(num)
+
+#Creat function in pyton 
+
+def my_function():
+  print("Hello from a function") 
